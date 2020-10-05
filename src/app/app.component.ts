7734d76124cc5ec3ce731a413adb0877d83cea5c
MedRiadhKhalfallah <mrk19933@gmail.com>
2020-10-05 21:18:38 +0100

    preparation login componenets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    var isNavbarTop = JSON.parse(localStorage.getItem('isNavbarTop'));
+  if (isNavbarTop) {
+    var navbarVertical = document.querySelector('.navbar-vertical');
+    var navbarTop = document.querySelector('.content .navbar-top');
+    navbarVertical.parentNode.removeChild(navbarVertical);
+    navbarTop.parentNode.removeChild(navbarTop);
+
+  } else {
+    var navbarTop = document.querySelector('[data-layout] .navbar-top');
+    navbarTop.parentNode.removeChild(navbarTop);
+  }
+  var isFluid = JSON.parse(localStorage.getItem('isFluid'));
+  if (isFluid) {
+    var container = document.querySelector('[data-layout]');
+    container.classList.remove('container');
+    container.classList.add('container-fluid');
+  }
+}
+
 }
