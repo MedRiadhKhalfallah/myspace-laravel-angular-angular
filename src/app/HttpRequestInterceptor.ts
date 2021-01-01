6f9e90e92da355574256b6ed2228e977a1fc079e
MedRiadhKhalfallah <mrk19933@gmail.com>
2021-01-01 20:20:59 +0100

    cron not active + test compte active + paginateur not active

---
+++ src/app/HttpRequestInterceptor.ts
@@ -0,0 +1,31 @@
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/internal/operators';
+import { Router } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {AuthService} from "./services/auth.service";
+import {TokenService} from "./services/token.service";
+
+/** Inject With Credentials into the request */
+@Injectable()
+export class HttpRequestInterceptor implements HttpInterceptor {
+
+  constructor(private auth: AuthService, private router: Router, private token: TokenService) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request)
+      .pipe(
+        catchError((err, caught: Observable<HttpEvent<any>>) => {
+          if (err instanceof HttpErrorResponse && err.status == 401) {
+            this.auth.changeAuthStatus(false);
+            this.token.remove();
+            localStorage.removeItem('roles');
+            localStorage.removeItem('user');
+            localStorage.removeItem('profileImg');
+            this.router.navigateByUrl('/login', { queryParams: { returnUrl: request.url } });
+          }
+          throw err;
+        })
+      );  }
+}
